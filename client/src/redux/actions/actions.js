d1b8541b5c439edf645b28d1ebe1ac7fe0bcc3eb
Tivani381 <t.ivani381@gmail.com>
2023-12-05 19:15:23 -0300

    1915

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -14,7 +14,7 @@ import {
   } from "./actions-types";
   import axios from "axios";
   
-  const { REACT_APP_URL_HOST } = 'https://deploy-production-479e.up.railway.app';
+  const REACT_APP_URL_HOST  = 'https://deploy-production-479e.up.railway.app';
   
   export const getDogs = () => {
     return async (dispatch) => {
