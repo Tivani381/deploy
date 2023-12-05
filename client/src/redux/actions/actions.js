d9a79437af1b035702098b405df393516fabb900
Tivani381 <t.ivani381@gmail.com>
2023-12-05 19:03:42 -0300

    1903

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -18,7 +18,7 @@ import {
   
   export const getDogs = () => {
     return async (dispatch) => {
-      const response = await axios.get(`${REACT_APP_URL_HOST}/dogs`);
+      const response = await axios.get(`${REACT_APP_URL_HOST}dogs`);
       return dispatch({
         type: GET_DOGS,
         payload: response.data,
@@ -29,7 +29,7 @@ import {
   export const getTemperaments = () => {
     return async (dispatch) => {
       try {
-        const response = await axios.get(`${REACT_APP_URL_HOST}/temps`);
+        const response = await axios.get(`${REACT_APP_URL_HOST}temps`);
         dispatch({
           type: GET_TEMPERAMENTS,
           payload: response.data
